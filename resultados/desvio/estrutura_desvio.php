8b20ccb3290dc81d879b58d0f4c35d1d39e33f4e
CaioBecker <83302532+CaioBecker@users.noreply.github.com>
2022-10-10 11:15:59 -0300

    filtro setor - desvio

--- resultados/desvio/estrutura_desvio.php
+++ resultados/desvio/estrutura_desvio.php
@@ -1,17 +1,28 @@
 <?php
 
     //LISTAR RESULTADO DESVIO
+    $var_setor = @$_POST["filtro_setor"];
 
-    $lista_resultado_desvio = "SELECT 'RECEITA/DESPESA' AS RESULTADO,
+    $lista_resultado_desvio = "SELECT total.ANO,
+                                total.MES,
+                                total.MES_ABV,
+                                SUM(total.VL_ORCADO) AS VL_ORCADO,
+                                SUM(total.VL_REALIZADO) AS VL_REALIZADO,
+                                SUM(total.VL_REALIZADO_ROUND) AS VL_REALIZADO_ROUND,
+                                SUM(total.VL_ORCADO_ROUND) AS VL_ORCADO_ROUND
+                            FROM (SELECT 'RECEITA/DESPESA' AS RESULTADO,
                                vrd.ANO, vrd.MES, vrd.MES_ABV, 
                                vrd.VL_ORCADO, vrd.VL_REALIZADO,
                                ROUND(NVL(vrd.VL_REALIZADO,0),0) AS VL_REALIZADO_ROUND,
                                ROUND(NVL(vrd.VL_ORCADO,0),0) AS VL_ORCADO_ROUND
-                               FROM orcamento_contabil.VW_RESULTADO_DESVIO vrd
+                               FROM orcamento_contabil.VW_RESULTADO_DESVIO_TESTE vrd
                                WHERE vrd.ANO = $var_ano
-                               AND vrd.CLASSIFICACAO_CONTABIL = UPPER('$var_visao')
+                               AND vrd.CLASSIFICACAO_CONTABIL = UPPER('$var_visao')";
+                               if($var_setor != 'Todos'){
+                                $lista_resultado_desvio .= " AND vrd.CD_SETOR = $var_setor";
+                               }
                                
-                               UNION ALL
+                               $lista_resultado_desvio .=" UNION ALL
                             
                                SELECT 'RESULTADO' AS RESULTADO,
                                rec.ANO, rec.MES, rec.MES_ABV, 
@@ -24,13 +35,16 @@
                                        FROM orcamento_contabil.VW_RESULTADO_DESVIO rec
                                        WHERE rec.ANO = $var_ano
                                        AND 'RESULTADO' = UPPER('$var_visao')
-                                       AND rec.CLASSIFICACAO_CONTABIL = UPPER('DESPESA')) dep
+                                       AND rec.CLASSIFICACAO_CONTABIL = UPPER('DESPESA')
+                                       ) dep
                                ON rec.ANO = dep.ANO
                                AND rec.MES = dep.MES
                                WHERE rec.ANO = $var_ano
                                AND 'RESULTADO' = UPPER('$var_visao')
                                AND rec.CLASSIFICACAO_CONTABIL = UPPER('RECEITA')
-                               ORDER BY 3 ASC";
+                                ORDER BY 3 ASC) total
+                                GROUP BY total.ANO, total.MES, total.MES_ABV
+                                ORDER BY 2";
 
     $result_resultado_desvio = oci_parse($conn_ora, $lista_resultado_desvio);
 
