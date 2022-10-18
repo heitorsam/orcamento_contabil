ac44c84832cf80af5b04afe7f87342b925948777
CaioBecker <83302532+CaioBecker@users.noreply.github.com>
2022-10-18 14:17:53 -0300

    Update modal_anexos_conta_contabil.php

--- modals/conta_contabil/modal_anexos_conta_contabil.php
+++ modals/conta_contabil/modal_anexos_conta_contabil.php
@@ -6,7 +6,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">Anexos </h5>
-                <button type="button" class="close" onclick="function (){ $('.modal-backdrop').remove() }" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" onclick="$('.modal-backdrop').remove()" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -18,7 +18,7 @@
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal"> <i class="fas fa-times"></i> Fechar</button>
+                <button type="button" class="btn btn-secondary" onclick="$('.modal-backdrop').remove()" data-dismiss="modal"> <i class="fas fa-times"></i> Fechar</button>
             </div>
         </div>
     </div>
