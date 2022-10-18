4f5f94049cb2d2a6a1b7f9bb630960bb344d43be
santacasasjc <scalabrinih@gmail.com>
2022-10-18 13:25:31 -0300

    Desvio e result prev

--- resultados/desvio/estrutura_desvio.php
+++ resultados/desvio/estrutura_desvio.php
@@ -3,14 +3,17 @@
     //LISTAR RESULTADO DESVIO
     $var_setor = @$_POST["filtro_setor"];
 
-    $lista_resultado_desvio = "SELECT total.ANO,
-                                total.MES,
-                                total.MES_ABV,
-                                SUM(total.VL_ORCADO) AS VL_ORCADO,
-                                SUM(total.VL_REALIZADO) AS VL_REALIZADO,
-                                SUM(total.VL_REALIZADO_ROUND) AS VL_REALIZADO_ROUND,
-                                SUM(total.VL_ORCADO_ROUND) AS VL_ORCADO_ROUND
-                            FROM (SELECT 'RECEITA/DESPESA' AS RESULTADO,
+    $lista_resultado_desvio = "SELECT res.*, np.VL_NECESSIDADE_PREVISTA,
+                               ROUND(NVL(np.VL_NECESSIDADE_PREVISTA,0), 0) AS VL_NECESSIDADE_PREVISTA_ROUND
+                               FROM(
+                               SELECT total.ANO,
+                               total.MES,
+                               total.MES_ABV,
+                               SUM(total.VL_ORCADO) AS VL_ORCADO,
+                               SUM(total.VL_REALIZADO) AS VL_REALIZADO,
+                               SUM(total.VL_REALIZADO_ROUND) AS VL_REALIZADO_ROUND,
+                               SUM(total.VL_ORCADO_ROUND) AS VL_ORCADO_ROUND
+                               FROM (SELECT 'RECEITA/DESPESA' AS RESULTADO,
                                vrd.ANO, vrd.MES, vrd.MES_ABV, 
                                vrd.VL_ORCADO, vrd.VL_REALIZADO,
                                ROUND(NVL(vrd.VL_REALIZADO,0),0) AS VL_REALIZADO_ROUND,
@@ -19,11 +22,11 @@
                                WHERE vrd.ANO = $var_ano
                                AND vrd.CLASSIFICACAO_CONTABIL = UPPER('$var_visao')";
                                if($var_setor != 'Todos'){
-                                $lista_resultado_desvio .= " AND vrd.CD_SETOR = $var_setor";
+                               $lista_resultado_desvio .= " AND vrd.CD_SETOR = $var_setor";
                                }
-                               
+                                                           
                                $lista_resultado_desvio .=" UNION ALL
-                            
+                                                           
                                SELECT 'RESULTADO' AS RESULTADO,
                                rec.ANO, rec.MES, rec.MES_ABV, 
                                NVL(rec.VL_ORCADO - dep.VL_ORCADO,0) AS VL_ORCADO,
@@ -32,20 +35,26 @@
                                ROUND(NVL(rec.VL_ORCADO - dep.VL_ORCADO,0),0) AS VL_ORCADO_ROUND
                                FROM orcamento_contabil.VW_RESULTADO_DESVIO rec
                                LEFT JOIN(SELECT rec.*
-                                       FROM orcamento_contabil.VW_RESULTADO_DESVIO rec
-                                       WHERE rec.ANO = $var_ano
-                                       AND 'RESULTADO' = UPPER('$var_visao')
-                                       AND rec.CLASSIFICACAO_CONTABIL = UPPER('DESPESA')
-                                       ) dep
+                                   FROM orcamento_contabil.VW_RESULTADO_DESVIO rec
+                                   WHERE rec.ANO = $var_ano
+                                   AND 'RESULTADO' = UPPER('$var_visao')
+                                   AND rec.CLASSIFICACAO_CONTABIL = UPPER('DESPESA')
+                                   ) dep
                                ON rec.ANO = dep.ANO
                                AND rec.MES = dep.MES
                                WHERE rec.ANO = $var_ano
                                AND 'RESULTADO' = UPPER('$var_visao')
                                AND rec.CLASSIFICACAO_CONTABIL = UPPER('RECEITA')
-                                ORDER BY 3 ASC) total
-                                GROUP BY total.ANO, total.MES, total.MES_ABV
-                                ORDER BY 2";
+                               ORDER BY 3 ASC) total
+                               GROUP BY total.ANO, total.MES, total.MES_ABV
+                               ORDER BY 2
+                               ) res
+                               LEFT JOIN orcamento_contabil.NECESSIDADE_PREVISTA np 
+                               ON np.PERIODO = res.MES || '/' || res.ANO
+                               ORDER BY res.ANO ASC, res.MES ASC";
 
+    //echo $lista_resultado_desvio;
+    
     $result_resultado_desvio = oci_parse($conn_ora, $lista_resultado_desvio);
 
 ?>
