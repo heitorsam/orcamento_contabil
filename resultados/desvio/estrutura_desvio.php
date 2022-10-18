156c04bd5096a5fa2e2f7692364b9db6e9f81858
santacasasjc <scalabrinih@gmail.com>
2022-10-18 08:43:21 -0300

    Revert "Update estrutura_desvio.php"
    This reverts commit 78b34ff743fdc2b6726ef3ed0e349c2b389effbe.

--- resultados/desvio/estrutura_desvio.php
+++ resultados/desvio/estrutura_desvio.php
@@ -19,38 +19,36 @@
                                WHERE vrd.ANO = $var_ano
                                AND vrd.CLASSIFICACAO_CONTABIL = UPPER('$var_visao')";
                                if($var_setor != 'Todos'){
-                                   $lista_resultado_desvio .= " AND vrd.CD_SETOR = $var_setor";
-                                }
-                                
-                                echo $lista_resultado_desvio .=" UNION ALL
-                                
-                                SELECT 'RESULTADO' AS RESULTADO,
-                                rec.ANO, rec.MES, rec.MES_ABV, 
-                                NVL(rec.VL_ORCADO - dep.VL_ORCADO,0) AS VL_ORCADO,
-                                NVL(rec.VL_REALIZADO - dep.VL_REALIZADO,0) AS VL_REALIZADO,
-                                ROUND(NVL(rec.VL_REALIZADO - dep.VL_REALIZADO,0),0) AS VL_REALIZADO_ROUND,
-                                ROUND(NVL(rec.VL_ORCADO - dep.VL_ORCADO,0),0) AS VL_ORCADO_ROUND
-                                FROM orcamento_contabil.VW_RESULTADO_DESVIO_TESTE rec
-                                LEFT JOIN(SELECT rec.*
-                                FROM orcamento_contabil.VW_RESULTADO_DESVIO_TESTE rec
-                                WHERE rec.ANO = $var_ano
-                                AND 'RESULTADO' = UPPER('$var_visao')
-                                AND rec.CLASSIFICACAO_CONTABIL = UPPER('DESPESA')
-                                AND rec.CD_SETOR = $var_setor
-                                ) dep
-                                ON rec.ANO = dep.ANO
-                                AND rec.MES = dep.MES
-                                WHERE rec.ANO = $var_ano
-                                AND 'RESULTADO' = UPPER('$var_visao')
-                                AND rec.CLASSIFICACAO_CONTABIL = UPPER('RECEITA')
-                                AND rec.CD_SETOR = $var_setor
+                                $lista_resultado_desvio .= " AND vrd.CD_SETOR = $var_setor";
+                               }
+                               
+                               $lista_resultado_desvio .=" UNION ALL
+                            
+                               SELECT 'RESULTADO' AS RESULTADO,
+                               rec.ANO, rec.MES, rec.MES_ABV, 
+                               NVL(rec.VL_ORCADO - dep.VL_ORCADO,0) AS VL_ORCADO,
+                               NVL(rec.VL_REALIZADO - dep.VL_REALIZADO,0) AS VL_REALIZADO,
+                               ROUND(NVL(rec.VL_REALIZADO - dep.VL_REALIZADO,0),0) AS VL_REALIZADO_ROUND,
+                               ROUND(NVL(rec.VL_ORCADO - dep.VL_ORCADO,0),0) AS VL_ORCADO_ROUND
+                               FROM orcamento_contabil.VW_RESULTADO_DESVIO rec
+                               LEFT JOIN(SELECT rec.*
+                                       FROM orcamento_contabil.VW_RESULTADO_DESVIO rec
+                                       WHERE rec.ANO = $var_ano
+                                       AND 'RESULTADO' = UPPER('$var_visao')
+                                       AND rec.CLASSIFICACAO_CONTABIL = UPPER('DESPESA')
+                                       ) dep
+                               ON rec.ANO = dep.ANO
+                               AND rec.MES = dep.MES
+                               WHERE rec.ANO = $var_ano
+                               AND 'RESULTADO' = UPPER('$var_visao')
+                               AND rec.CLASSIFICACAO_CONTABIL = UPPER('RECEITA')
                                 ORDER BY 3 ASC) total
                                 GROUP BY total.ANO, total.MES, total.MES_ABV
                                 ORDER BY 2";
-                                
-                                $result_resultado_desvio = oci_parse($conn_ora, $lista_resultado_desvio);
-                                
-                                ?>
+
+    $result_resultado_desvio = oci_parse($conn_ora, $lista_resultado_desvio);
+
+?>
 
 <div class="fnd_azul" id="fnd_azul">     
     <i class="fas fa-chart-line"></i> Desvio <?php echo $var_visao . ' ' . $var_ano; ?>
